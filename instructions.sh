646e5cc37213f4cc8ef3acc7145b88cd6631f5d7
Marwa98M <mm1604391@gmail.com>
2024-04-07 04:05:39 +0300

    Clone all files

--- instructions.sh
+++ instructions.sh
@@ -4,10 +4,11 @@ docker build -t kaggle-downloader .
 docker run -v $(pwd)/data:/data -e KAGGLE_USERNAME=$KAGGLE_USERNAME -e KAGGLE_KEY=$KAGGLE_KEY kaggle-downloader
 
 git checkout -b train
-cp -r ../data ./data
+# cp -r ../data ./data
 
 ## Train
-sudo unzip ./data/100-bird-species.zip -d ./datadata/
+sudo unzip ./data/100-bird-species.zip -d ./data/
 
 docker build -t bird-classification .
 docker run bird-classification
+
